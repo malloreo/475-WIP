76d2d64ffde5d475c0cf134fba54a332bc9c4124
Chanamon <cratanal@andrew.cmu.edu>
2014-11-30 23:02:28 -0500

    chat formatting
    - right aligned for you
    - formatting chat “bubbles”
    - adding time conditionals

--- public/js/script_chat.js
+++ public/js/script_chat.js
@@ -1,7 +1,12 @@
 $(function() {
-	$(document).ready(getChats);
-	$("#button").click(addMessage)
+	getUser();
+	getChats();
+	console.log("top: "+document.getElementById('chat-messages').scrollTop) 
+	console.log("height: "+document.getElementById('chat-messages').scrollHeight) 
+	$("#chat-button").click(addMessage)
 })
+
+this_user = "";
 
 function addMessage(){
 	message = $("#message").val()
@@ -17,9 +22,10 @@ function addMessage(){
 				date_time_str = date.toLocaleString()
 				// date_time_str = date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
 				// $('#chat-messages').append(data.user.name +" <i>[" + date_time_str + "]</i> : "+ message+"<br>");
-				$('#chat-messages').append(data.user.name + " <i>[" + date_time_str + "]</i><br>" + message + "<br><br>");
+				$('#chat-messages').append(str += "<div id='chat-me'>"+"<span>" + " <i>" + date_time_str + "</i></span><br>" + message + "</div>");
 				$('#message').val("");
 				$("#message").focus()
+				document.getElementById('chat-messages').scrollTop = document.getElementById('chat-messages').scrollHeight
 			}
 		});
 	}
@@ -27,6 +33,8 @@ function addMessage(){
 }
 
 function getChats(){
+	prevAuthor = ""
+	prevTime = ""
 	$.ajax({
 		url: "getChats",
 		type: "get",
@@ -36,12 +44,38 @@ function getChats(){
 			data.forEach(function(d){
 				date = new Date(d.date)
 				date_time_str = date.toLocaleString()
+				if (d.author == this_user){ //you
+					str += "<div id='chat-me'>"+"<span>" + " <i>" + date_time_str + "</i></span><br>" + d.message + "</div>"
+				} else { // someone else
+					if (prevAuthor == d.author){ //same chatter as before
+						if ((date.getMinutes()-prevTime.getMinutes())>3){ //show time again
+							str += "<div id='chat-them'><span><i>" + date_time_str + "</i></span><br>" + d.message + "</div>"
+						} else { // just show message
+							str += "<div id='chat-them'>" + d.message + "</div>"
+						}
+					} else {
+						str += "<div id='chat-them'><span><p>"+d.author + "</p> <i>" + date_time_str + "</i></span><br>" + d.message + "</div>"
+					}
+				}
 				// str += d.author +" <i>[" + date_time_str + "]</i> : "+ d.message+"<br>"
-				str += d.author + " <i>[" + date_time_str + "]</i><br>" + d.message + "<br><br>"
+				prevAuthor = d.author
+				prevTime = date
 			})
 			
 			$("#chat-messages").append(str)
+			document.getElementById('chat-messages').scrollTop = document.getElementById('chat-messages').scrollHeight
 		}
 	})
 	return false;
+}
+
+function getUser(){
+	$.ajax({
+		url: "getUser",
+		type: "get",
+		data: {},
+		success: function(data){
+			this_user = data.name
+		}
+	})
 }
