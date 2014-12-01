a2afc2456eb23641e36430ae322221128856f68c
Chanamon <cratanal@andrew.cmu.edu>
2014-11-30 23:25:12 -0500

    more chat changes

--- public/js/script_chat.js
+++ public/js/script_chat.js
@@ -46,21 +46,31 @@ function getChats(){
 				date = new Date(d.date)
 				date_time_str = date.toLocaleString()
 				if (d.author == this_user){ //you
-					str += "<div id='chat-me'>"+"<span>" + " <i>" + date_time_str + "</i></span><br>" + d.message + "</div>"
+					if ((prevAuthor == d.author)&&((date.getMinutes()-prevTime)<3)){ //show time again
+						str += "<div id='chat-me'>" + d.message + "</div>"
+						console.log("1 - "+d.message);
+					} else {
+						str += "<div id='chat-me'>"+"<span>" + " <i>" + date_time_str + "</i></span><br>" + d.message + "</div>"
+						console.log("2 - "+d.message);
+					}
 				} else { // someone else
 					if (prevAuthor == d.author){ //same chatter as before
-						if ((date.getMinutes()-prevTime.getMinutes())>3){ //show time again
-							str += "<div id='chat-them'><span><i>" + date_time_str + "</i></span><br>" + d.message + "</div>"
+						if ((date.getMinutes()-prevTime)>3){ //show time again
+						console.log("3 - "+d.message);
+							str += "<div id='chat-them'><span><p>"+d.author + "</p> <i>" + date_time_str + "</i></span><br>" + d.message + "</div>"
 						} else { // just show message
+						console.log("4 - "+d.message);
 							str += "<div id='chat-them'>" + d.message + "</div>"
 						}
 					} else {
+						console.log("5 - "+d.message);
 						str += "<div id='chat-them'><span><p>"+d.author + "</p> <i>" + date_time_str + "</i></span><br>" + d.message + "</div>"
 					}
 				}
 				// str += d.author +" <i>[" + date_time_str + "]</i> : "+ d.message+"<br>"
 				prevAuthor = d.author
-				prevTime = date
+				console.log("prevauthor: ", prevAuthor)
+				prevTime = date.getMinutes();
 			})
 			
 			$("#chat-messages").html(str)
